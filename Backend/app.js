b10929af110cf49cfaa47661e1df3fce42d286b8
bousra1335 <bousrajahanlia@gmail.com>
2024-02-26 11:42:34 +0600

    app.js file will be used for routes

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 dotenv.config({path: "./config/config.env"});
@@ -18,4 +19,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+
+    })
+);
 export default app;
