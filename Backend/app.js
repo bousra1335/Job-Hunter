0ff32aa52dfbb1759ad5942b6d624ff50094384d
bousra1335 <bousrajahanlia@gmail.com>
2024-02-26 11:36:05 +0600

    Importing cookie-parser

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config({path: "./config/config.env"});
@@ -14,6 +14,7 @@ app.use(
     })
 );
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
