9ab2dada2c1ecdaa4b3bec4e06f2506d2d537a9c
bousra1335 <bousrajahanlia@gmail.com>
2024-03-01 23:14:26 +0600

    Finished backend code of authorization

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {login,logout,register} from '../controllers/userController.js';
+import {isAuthorized} from "../middlewares/auth.js";
 
 const router= express.Router();
 
 router.post("/register",register);
 router.post("/login",login);
-router.post("/logout",logout);
+router.post("/logout",isAuthorized, logout);
 
 export default router;
