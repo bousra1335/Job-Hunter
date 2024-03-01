855c29549c8a04460631e5ba1b114c5d2488cf13
bousra1335 <bousrajahanlia@gmail.com>
2024-03-01 21:19:46 +0600

    setting authorization in user controller file

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,3 +1,27 @@
-export const login = ()=> {
-    
-}
+import {catchAsyncError} from '../middlewares/catchAsyncError.js';
+import ErrorHandler from '../middlewares/error.js';
+import { User } from '../models/userSchema.js';
+
+export const login = catchAsyncError(async(req, res,next)=> {
+    const {name, email, phone,role,password} = req.body;
+    if (!name  || !email || !phone || !password){
+        return next(new ErrorHandler("Please fill full registration form!"));
+
+    }
+    const isEmail = await User.findOne({email});
+    if(isEmail){
+        return next(new ErrorHandler("Email already exists!"));
+    }
+    const user= await User.create({
+        name, 
+        email, 
+        phone,
+        role,
+        password,
+    });
+    res.status(200).json({
+        success: true,
+        message: "User registered!",
+        user,
+    })
+});
