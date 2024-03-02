9b367ede898ab3e2094f8bc587f5ab5d82f87ac2
bousra1335 <bousrajahanlia@gmail.com>
2024-03-02 12:17:33 +0600

    Creating files in Job folder

---
+++ Frontend/src/components/Job/JobDetails.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const JobDetails = () => {
+    return{
+
+    }
+}
+export default JobDetails
---
+++ Frontend/src/components/Job/PostJob.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PostJob = () => {
+    return{
+
+    }
+}
+export default PostJob
