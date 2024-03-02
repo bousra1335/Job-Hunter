a7e1285ce0875bab0c1081a1e86fbd4a99dea1af
Keya <kskkeya@gmail.com>
2024-03-02 15:53:11 +0600

    1st

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { Context } from "./main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Home from "./components/Home/Home";
+import Jobs from "./components/Job/Jobs";
+import JobDetails from "./components/Job/JobDetails";
+import MyJobs from "./components/Job/MyJobs";
+import PostJob from "./components/Job/PostJob";
+import Application from "./components/Application/Application";
+import MyApplications from "./components/Application/MyApplications";
+import NotFound from "./components/NotFound/NotFound";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
+      }
+    };
+    fetchUser();
+  }, [isAuthorized]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+          <Route path="/application/:id" element={<Application />} />
+          <Route path="/applications/me" element={<MyApplications />} />
+          <Route path="/job/post" element={<PostJob />} />
+          <Route path="/job/me" element={<MyJobs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Toaster />
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 
+export const Context = createContext({ isAuthorized: false });
+
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized]= useState(false);
+  const [user, setUser] = useState({});
+
+  return(
+    <Context.Provider value={{isAuthorized, setIsAuthorized,user, setUser}}>
+      <App />
+
+    </Context.Provider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
-)
+);
