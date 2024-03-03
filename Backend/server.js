169f3b6ea66d5ae6b07702656ae5e7d136a44a3c
bousra1335 <bousrajahanlia@gmail.com>
2024-03-03 12:55:51 +0600

    solving problems

--- Backend/server.js
+++ Backend/server.js
@@ -8,5 +8,5 @@ cloudinary.v2.config({
 });
 
 app.listen(process.env.PORT,() =>{
-    console.log("Server running on port ${process.env.PORT}");
+    console.log(`Server running on port ${process.env.PORT}`);
 });
