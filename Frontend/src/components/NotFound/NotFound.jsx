4b70b44e6b3e89ad70c9ca76fd94506dc429b2bb
Keya <kskkeya@gmail.com>
2024-03-03 21:48:25 +0600

    logo change

--- Frontend/src/components/NotFound/NotFound.jsx
+++ Frontend/src/components/NotFound/NotFound.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <>
-        <section className='page notfound'>
-          <div className="content">
-            <img src="/notfound.png" alt="notfound" />
+    <section className='page notfound'>
+        <div className="content">
+            <img src="/not found.png" alt="not found" />
             <Link to={'/'}>RETURN TO HOME PAGE</Link>
-          </div>
-        </section>
-    </>
-  )
-}
+        </div>
+    </section>
+    
+  );
 
-export default NotFound
+};
+
+export default NotFound;
