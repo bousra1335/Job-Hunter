1239ece19732f35685b0aaa11838c4975289d145
Keya <kskkeya@gmail.com>
2024-03-03 12:36:09 +0600

    1st

--- Backend/middlewares/error.js
+++ Backend/middlewares/error.js
@@ -1,7 +1,7 @@
 class ErrorHandler extends Error{
     constructor(message, statusCode){
         super(message);
-        this.statusbar = statusCode;
+        this.statusCode = statusCode;
     
     }
 }
