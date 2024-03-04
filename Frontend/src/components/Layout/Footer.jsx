97e92965a583cd309d0a30dc255a041da678e965
Keya <kskkeya@gmail.com>
2024-03-04 19:48:04 +0600

    Updated Some Changes in src/components/home

--- Frontend/src/components/Layout/Footer.jsx
+++ Frontend/src/components/Layout/Footer.jsx
@@ -2,20 +2,20 @@ import React, { useContext } from "react";
 import { Context } from "../../main";
 import { Link } from "react-router-dom";
 import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
+
 
 const Footer = () => {
   const { isAuthorized } = useContext(Context);
   return (
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
       <div>
-        <Link to={"https://www.facebook.com/keya.ksk/"} target="_blank">
+        <Link to={""} target="_blank">
           <FaFacebookF />
         </Link>
-        <Link to={"https://www.linkedin.com/in/kawser-sultana-039231258/"} target="_blank">
+        <Link to={""} target="_blank">
           <FaLinkedin />
         </Link>
-        <Link to={"https://www.youtube.com/@KAWSERSULTANA"} target="_blank">
+        <Link to={""} target="_blank">
           <FaYoutube />
         </Link>
       </div>
