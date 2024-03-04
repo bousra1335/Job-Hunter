e512cd246c9dcb2de16e72c72f1b30b1d803e728
bousra1335 <bousrajahanlia@gmail.com>
2024-03-04 20:40:52 +0600

    Solving errors  in register.jsx file

--- Frontend/src/components/Auth/Register.jsx
+++ Frontend/src/components/Auth/Register.jsx
@@ -7,7 +7,7 @@ import {FaPencilAlt} from "react-icons/fa";
 import {MdOutlineMailOutline} from "react-icons/md";
 import {FaPhoneFlip} from "react-icons/fa6";
 import {RiLock2Fill} from "react-icons/ri";
-import { Link, Navigate } from "react-router-dom";
+import { Link as RouterLink, Navigate as RouterNavigate } from "react-router-dom";
 
 
 const Register = () => {
