c59a43cfd9f6b6129b56fc1743a5a81d23282a05
bousra1335 <bousrajahanlia@gmail.com>
2024-03-16 22:23:40 +0600

    Import Post Job and Get Job in jobRouter.js file

--- Backend/controllers/jobController.js
+++ Backend/controllers/jobController.js
@@ -10,3 +10,65 @@ export const getAllJobs = catchAsyncError(async (req, res, next) => {
   });
 });
 
+export const postJob = catchAsyncError(async (req, res, next) => {
+  const {role} = req.user;
+  if (role === "Job Seeker"){
+    return next(
+      new ErrorHandler(
+        "Job Seeker not allowed to access this resources!",
+        400
+      )
+    );
+  }
+  const {
+    title,
+    description,
+    category,
+    country,
+    city,
+    location,
+    fixedSalary,
+    salaryFrom,
+    salaryTo
+  } = req.body;
+
+  if(!title || !description || !category || !country|| !city|| !location){
+    return next(new ErrorHandler("Please provide full job details",400));
+  }
+
+  if ((!salaryFrom || !salaryTo ) && !fixedSalary) {
+    return next(
+      new ErrorHandler(
+        "Please either provide fixed salary or ranged salary.",
+        400
+      )
+    );
+  }
+  if (salaryFrom && salaryTo && fixedSalary){
+    return next(
+      new ErrorHandler("Cannot Enter Fixed and Ranged Salary together.",400)
+
+    );
+  }
+  const postedBy = req.user._id;
+  const job = await Job.create({
+    title,
+    description,
+    category,
+    country,
+    city,
+    location,
+    fixedSalary,
+    salaryFrom,
+    salaryTo,
+    postedBy
+  })
+
+  res.status(200).json({
+    success: true,
+    message: "Job posted successfully",
+    job
+  })
+
+});
+
--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,8 +1,15 @@
 import express from "express";
-import {getAllJobs} from '../controllers/jobController.js'
+import {
+    getAllJobs,
+    postJob,
+}   from "../controllers/jobController.js";
+
+import {isAuthorized} from '../middlewares/auth.js';
 import { get } from "mongoose";
+
 const router= express.Router();
 
 router.get("/getall",getAllJobs);
+router.post("/post", isAuthorized, postJob);
 
 export default router;
