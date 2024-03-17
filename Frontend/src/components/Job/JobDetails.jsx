6550a664f6ff5dd1ba33d562f0d67908c8e84bbe
bousra1335 <bousrajahanlia@gmail.com>
2024-03-17 13:26:33 +0600

    Creating Job Details page

--- Frontend/src/components/Job/JobDetails.jsx
+++ Frontend/src/components/Job/JobDetails.jsx
@@ -1,5 +1,5 @@
 import React,{useContext,useEffect,useState} from 'react';
-import {useNavigate,useParams} from 'react-router-dom';
+import {Link, useNavigate,useParams} from 'react-router-dom';
 import {Context} from "../../main";
 import axios from 'axios';
 
@@ -9,25 +9,67 @@ const JobDetails = () => {
     const navigateTo = useNavigate();
 
     const {isAuthorized, user} = useContext(Context);
+
     useEffect(()=>{
         axios
             .get(`http://localhost:4000/api/v1/job/${id}`, {
                 withCredentials: true,
-            }).then(res=>{
+            })
+            .then(res=>{
                 setJob(res.data.job);
-            }).catch(err=>{
-                console.log(err.response.data.json);
+            })
+            .catch((error)=>{
+                navigateTo("/notfound");
             });
     },[]);
 
     if(!isAuthorized){
         navigateTo("/login");
     }
-    return(
-    <>
-    </>
-    )
-
-    
+    return (
+    <section className='jobDetail page'>
+        <div className='container'>
+            <h3>Job Details</h3>
+            <div className='banner'>
+                <p>
+                    Title: <span>{job.title}</span>
+                </p>
+                <p>
+                    Category: <span>{job.category}</span>
+                </p>
+                <p>
+                    Country: <span>{job.country}</span>
+                </p>
+                <p>
+                    City: <span>{job.city}</span>
+                </p>
+                <p>
+                    Location: <span>{job.location}</span>
+                </p>
+                <p>
+                    Description: <span>{job.description}</span>
+                </p>
+                <p>
+                    Job Posted On: <span>{job.jobPostedOn}</span>
+                </p>
+                <p>
+                    Salary: {" "}
+                    {job.fixedSalary ? (
+                        <span>{job.fixedSalary}</span>
+                    ):(
+                        <span>
+                            {job.salaryFrom} - {job.salaryTo}
+                        </span>
+                    )}
+                </p>
+                    {user && user.role === "Employer" ? (
+                        <></>
+                    ): (
+                        <Link to={`/application/${job._id}`}>Apply Now</Link>
+                    )}
+            </div>
+        </div>
+    </section>
+    );
 };
 export default JobDetails;
