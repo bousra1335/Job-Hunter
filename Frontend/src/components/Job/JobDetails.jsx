5fb2e6ea5ebe69b27e96ed1504528631d3a992f8
bousra1335 <bousrajahanlia@gmail.com>
2024-03-17 12:42:35 +0600

    Setting JobDetails.jsx file, adding navigate, setJob options

--- Frontend/src/components/Job/JobDetails.jsx
+++ Frontend/src/components/Job/JobDetails.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React,{useContext,useEffect,useState} from 'react';
+import {useNavigate,useParams} from 'react-router-dom';
+import {Context} from "../../main";
+import axios from 'axios';
 
 const JobDetails = () => {
-    return<></>;
+    const {id}= useParams();
+    const [job, setJob] = useState({});
+    const navigateTo = useNavigate();
+
+    const {isAuthorized, user} = useContext(Context);
+    useEffect(()=>{
+        axios
+            .get(`http://localhost:4000/api/v1/job/${id}`, {
+                withCredentials: true,
+            }).then(res=>{
+                setJob(res.data.job);
+            }).catch(err=>{
+                console.log(err.response.data.json);
+            });
+    },[]);
+
+    if(!isAuthorized){
+        navigateTo("/login");
+    }
+    return(
+    <>
+    </>
+    )
 
     
 };
