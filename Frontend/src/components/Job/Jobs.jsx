060006d6e78175458dbb4379396e6d0fffc7103e
bousra1335 <bousrajahanlia@gmail.com>
2024-03-17 11:26:50 +0600

    Creating All Job page and for that working on Jobs.jsx in Frontend and jobcontrollers.js and jobRoutes.js in Backend

--- Frontend/src/components/Job/Jobs.jsx
+++ Frontend/src/components/Job/Jobs.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Context } from '../../main';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Jobs = () => {
-    return<></>;
+    const [jobs, setJobs] = useState([]);
+    const {isAuthorized} = useContext(Context);
+    const navigateTo = useNavigate();
+    useEffect (()=>{
+        try{
+            axios
+                .get("http://localhost:4000/api/v1/job/getAll",{
+                    withCredentials:true
+                })
+                .then((res)=>{
+                    setJobs (res.data);
+                });
+        }   catch(error){
+            console.log(error);
+        }
+    }, []);
+    if (!isAuthorized){
+        navigateTo("/login");
+    }
+    return<>
+        <section className="jobs page">
+            <div className='container'>
+                <h1>ALL AVAILABLE JOBS</h1>
+                <div className='banner'>
+                    {
+                        jobs.jobs && jobs.jobs.map((element) =>{
+                            return (
+                                <div className='card' key={element._id}>
+                                    <p>{element.title}</p>
+                                    <p>{element.category}</p>
+                                    <p>{element.country}</p>
+                                    <Link to={`/job/${element._id}`}>Job Details</Link>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </section>
+    </>;
+    
     
 };
 export default Jobs;
