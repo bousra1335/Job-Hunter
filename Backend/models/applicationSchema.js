1b9b0736660fb2bf7fb5cb5bab481d95ba2fcb67
Keya <kskkeya@gmail.com>
2024-03-21 21:33:19 +0600

    Created appliactionSchema.js for Application pages in model in Backend

---
+++ Backend/models/applicationSchema.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const applicationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name!"],
+        minLength: [3, "Name must contain at least 3 character"],
+        maxLength: [30, "Name cannot exceed 30 character"],
+    },
+    email: {
+        type: String,
+        validator: [validator.isEmail, "Please provide a valid email!"],
+        required: [true, "Please provide your email!"],
+    },
+    coverLetter: {
+        type: String,
+        required: [true, "Please provide your cover Letter!"],
+    },
+    phone: {
+        type: Number,
+        required: [true, "Please provide your Phone Number!"],
+    },
+    address: {
+        type: String,
+        required: [true, "Please provide your Address!"],
+    },
+    resume: {
+        publice_id:{
+            type: String,
+            required: true
+        },
+        url:{
+            type: String,
+            required: true
+        },
+    },
+    applicantID:{
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required: true
+        },
+        role:{
+            type: String,
+            enum: ["Job Seeker"],
+            required: true
+        },
+    },
+    employerID:{
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required: true
+        },
+        role:{
+            type: String,
+            enum: ["Employer"],
+            required: true
+        },
+    },
+});
+
+export const Application= mongoose.model("Application", applicationSchema);
