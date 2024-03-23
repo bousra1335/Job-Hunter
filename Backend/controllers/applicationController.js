98bfb2c65ece74a2ad0c177ec3babda299d35857
Keya <kskkeya@gmail.com>
2024-03-24 03:56:44 +0600

    modified applicationController.js and applicationRouter.js for application pages in backend

--- Backend/controllers/applicationController.js
+++ Backend/controllers/applicationController.js
@@ -1,7 +1,8 @@
 import {catchAsyncError} from '../middlewares/catchAsyncError.js'
 import ErrorHandler from '../middlewares/error.js'
 import { Application } from '../models/applicationSchema.js';
-
+import cloudinary from 'cloudinary';
+import { Job } from "../models/jobSchema.js";
 
 export const employerGetAllApplications = catchAsyncError(async(req, res, next)=>{
     const {role} = req.user;
@@ -19,7 +20,7 @@ export const employerGetAllApplications = catchAsyncError(async(req, res, next)=
 });
 
 export const jobseekerGetAllApplications = catchAsyncError(async(req, res, next)=>{
-    const {role} = req.user;
+  const {role} = req.user;
   if (role === "Employer"){
     return next(
       new ErrorHandler("Employer is not allowed to access this resource.",400)
@@ -53,3 +54,74 @@ export const jobSeekerDeleteApplication = catchAsyncError(
       });
     }
   );
+
+  export const postApplication = catchAsyncError(async(req,res,next)=>{
+    const { role } = req.user;
+      if (role === "Employer") {
+        return next(
+          new ErrorHandler("Employer not allowed to access this resources!", 400)
+        );
+      }
+      if(!req.files || Object.keys(req.files).length === 0){
+        return next(new ErrorHandler("Resume File Required",400));
+      }
+      const {resume} = req.files;
+      const allowedFormats = ['image/png', 'image/jpg', 'image/webp'];
+      if (!allowedFormats.includes(resume.minetype)){
+        return next(new ErrorHandler("Invalid file type. Please upload your resume in a PNG, JPG or WEBP format.",400))
+      }
+      const cloudinaryResponse = await cloudinary.uploader.upload(
+        resume.tempFilePath
+      );
+      console.log(cloudinaryResponse)
+      if (!cloudinaryResponse || cloudinaryResponse.error){
+        console.error("Cloudinary Error:", cloudinaryResponse.error || "Unknown cloudinary Error");
+        return next(new ErrorHandler("Failed to upload resume:", 500))
+      }
+      const {name, email, coverLetter, phone, address, jobId}= req.body;
+      const applicantID = {
+        user: req.user._id,
+        role: "Job Seeker",
+      };
+      if (!jobId) {
+        return next(new ErrorHandler("Job not found!", 404));
+      }
+      const jobDetails = await Job.findById(jobId);
+      if (!jobDetails) {
+        return next(new ErrorHandler("Job not found!", 404));
+      }
+      const employerID = {
+        user: jobDetails.postedBy,
+        role: "Employer",
+      };
+      if (
+        !name ||
+        !email ||
+        !coverLetter ||
+        !phone ||
+        !address ||
+        !applicantID ||
+        !employerID ||
+        !resume
+      ) {
+        return next(new ErrorHandler("Please fill all fields!", 400));
+      }
+      const application = await Application.create({
+        name,
+        email,
+        coverLetter,
+        phone,
+        address,
+        applicantID,
+        employerID,
+        resume: {
+          public_id: cloudinaryResponse.public_id,
+          url: cloudinaryResponse.secure_url,
+        },
+      });
+      res.status(200).json({
+        success: true,
+        message: "Application Submitted!",
+        application,
+      });
+  });
--- Backend/routes/applicationRouter.js
+++ Backend/routes/applicationRouter.js
@@ -3,6 +3,7 @@ import {
     employerGetAllApplications,
     jobSeekerDeleteApplication,
     jobseekerGetAllApplications,
+    postApplication,
 } from "../controllers/applicationController.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
@@ -12,4 +13,6 @@ const router= express.Router();
 router.get("/jobseeker/getall",isAuthorized, jobseekerGetAllApplications);
 router.get("/employer/getall",isAuthorized, employerGetAllApplications);
 router.delete("/delete/:id", isAuthorized, jobSeekerDeleteApplication);
+router.post("/post", isAuthorized, postApplication);
+
 export default router;
