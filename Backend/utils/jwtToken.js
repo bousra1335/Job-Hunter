561d50fd58aac59a7df99e4969fedc5835ffedf6
Keya <kskkeya@gmail.com>
2024-03-27 12:16:16 +0600

    Solved post application problem

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,9 +1,7 @@
 export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken();
     const options= {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24*60*60*1000
-        ),
+        expires: '1h',
         httpOnly: true,
     };
     res.status(statusCode).cookie("token",token,options).json({
