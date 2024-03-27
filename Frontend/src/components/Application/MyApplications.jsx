c0b2a8f5ffc0045fd7b1aa5e9e2246f6eacb6f9e
Aber Islam <71089269+iamaber@users.noreply.github.com>
2024-03-27 23:18:47 +0600

    Added EmployerCard function to MyApplications.jsx

--- Frontend/src/components/Application/MyApplications.jsx
+++ Frontend/src/components/Application/MyApplications.jsx
@@ -159,3 +159,36 @@ const JobSeekerCard = ({ element, deleteApplication, openModal }) => {
     </>
   );
 }; 
+
+const EmployerCard = ({ element, openModal }) => {
+  return (
+    <>
+      <div className="job_seeker_card">
+        <div className="detail">
+          <p>
+            <span>Name:</span> {element.name}
+          </p>
+          <p>
+            <span>Email:</span> {element.email}
+          </p>
+          <p>
+            <span>Phone:</span> {element.phone}
+          </p>
+          <p>
+            <span>Address:</span> {element.address}
+          </p>
+          <p>
+            <span>CoverLetter:</span> {element.coverLetter}
+          </p>
+        </div>
+        <div className="resume">
+          <img
+            src={element.resume.url}
+            alt="resume"
+            onClick={() => openModal(element.resume.url)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
