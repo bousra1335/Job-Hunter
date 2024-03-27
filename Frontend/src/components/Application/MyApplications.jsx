7e31c1056938147eedf33e34647bf3c8912ce99b
Aber Islam <71089269+iamaber@users.noreply.github.com>
2024-03-27 23:17:44 +0600

    Created JobSeekerCard function in MyApplications.jsx

--- Frontend/src/components/Application/MyApplications.jsx
+++ Frontend/src/components/Application/MyApplications.jsx
@@ -119,3 +119,43 @@ const MyApplications = () => {
 };
 
 export default MyApplications;
+
+export default MyApplications;
+
+const JobSeekerCard = ({ element, deleteApplication, openModal }) => {
+  return (
+    <>
+      <div className="job_seeker_card">
+        <div className="detail">
+          <p>
+            <span>Name:</span> {element.name}
+          </p>
+          <p>
+            <span>Email:</span> {element.email}
+          </p>
+          <p>
+            <span>Phone:</span> {element.phone}
+          </p>
+          <p>
+            <span>Address:</span> {element.address}
+          </p>
+          <p>
+            <span>CoverLetter:</span> {element.coverLetter}
+          </p>
+        </div>
+        <div className="resume">
+          <img
+            src={element.resume.url}
+            alt="resume"
+            onClick={() => openModal(element.resume.url)}
+          />
+        </div>
+        <div className="btn_area">
+          <button onClick={() => deleteApplication(element._id)}>
+            Delete Application
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}; 
